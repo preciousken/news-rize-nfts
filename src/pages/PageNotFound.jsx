48bacb3be8f92ae1e19af374a84bbb4674aada09
preciousken <pkehinde27@gmail.com>
2024-09-08 16:08:40 +0100

    added the 404 page

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -4,7 +4,7 @@ import "./css/404.css";
 
 const PageNotFound = () => {
   return (
-    <div className="section">
+    <div className="errorpage section">
       <h1 className="error">404</h1>
       <div className="page">
         Ooops!!! The page you are looking for is not found
